9e2e162ef7dad1fc705d4591393803bae1517ae3
Tan Shi Bei <tshibei@nus.edu.sg>
2022-04-28 13:32:43 +0800

    Add potential code for defining multiple dicts

--- Retro Pediatric Liver Transplant.py
+++ Retro Pediatric Liver Transplant.py
@@ -31,38 +31,58 @@ warnings.simplefilter('ignore', np.RankWarning)
 from scipy.optimize import curve_fit
 import matplotlib.patches as patches
 
+sections = ['main', 'errdict', 'excdict']
+section_dict = {sec : {} for sec in sections}
+section_dict['main']
+
+
 # +
 from scipy.optimize import curve_fit
 input_file = 'Retrospective Liver Transplant Data.xlsx'
 
 # Create dictionaries to store information for individual patients
-df = {}
-quad_cal_pred = {}
-linear_cal_pred = {}
-df_Q_Cum = {}
-df_Q_Cum_input = {}
-df_Q_Cum_origin_int_input = {}
-df_Q_Cum_origin_int = {}
-df_Q_PPM = {}
-df_Q_PPM_origin_int = {}
-df_L_Cum = {}
-df_L_Cum_input = {}
-df_L_Cum_origin_int = {}
-df_L_Cum_origin_int_input = {}
-df_L_PPM = {}
-df_L_PPM_origin_int = {}
-df_Q_RW_input = {}
-df_Q_RW = {}
-df_L_RW_input = {}
-df_L_RW = {}
-df_RW = {}
-df_Q_Cum_origin_dp_input = {}
-df_Q_Cum_origin_dp = {}
-df_L_Cum_origin_dp_input = {}
-df_L_Cum_origin_dp = {}
-df_Q_PPM_origin_dp = {}
-df_L_PPM_origin_dp = {}
-df_PPM_origin_dp = {}
+# df = {}
+# quad_cal_pred = {}
+# linear_cal_pred = {}
+# df_Q_Cum = {}
+# df_Q_Cum_input = {}
+# df_Q_Cum_origin_int_input = {}
+# df_Q_Cum_origin_int = {}
+# df_Q_PPM = {}
+# df_Q_PPM_origin_int = {}
+# df_L_Cum = {}
+# df_L_Cum_input = {}
+# df_L_Cum_origin_int = {}
+# df_L_Cum_origin_int_input = {}
+# df_L_PPM = {}
+# df_L_PPM_origin_int = {}
+# df_Q_RW_input = {}
+# df_Q_RW = {}
+# df_L_RW_input = {}
+# df_L_RW = {}
+# df_RW = {}
+# df_Q_Cum_origin_dp_input = {}
+# df_Q_Cum_origin_dp = {}
+# df_L_Cum_origin_dp_input = {}
+# df_L_Cum_origin_dp = {}
+# df_Q_PPM_origin_dp = {}
+# df_L_PPM_origin_dp = {}
+# df_PPM_origin_dp = {}
+
+# dicts = ['df', 'quad_cal_pred', 'linear_cal_pred', 
+#         'df_Q_Cum', 'df_Q_Cum_input', 'df_Q_Cum_origin_int_input', 'df_Q_Cum_origin_int',
+#         'df_Q_PPM', 'df_Q_PPM_origin_int',
+#         'df_L_Cum', 'df_L_Cum_input', 'df_L_Cum_origin_int', 'df_L_Cum_origin_int_input',
+#         'df_L_PPM', 'df_L_PPM_origin_int',
+#         'df_Q_RW_input', 'df_Q_RW', 'df_L_RW_input', 'df_L_RW', 'df_RW',
+#         'df_Q_Cum_origin_dp_input', 'df_Q_Cum_origin_dp',
+#         'df_L_Cum_origin_dp_input', 'df_L_Cum_origin_dp',
+#         'df_Q_PPM_origin_dp', 'df_L_PPM_origin_dp', 'df_PPM_origin_dp']
+
+# # for y in enumerate(dicts):
+# #     y = {}
+
+# all_dicts = {each_dict : {} for each_dict in dicts}
 
 # Define lists and parameters
 patients_to_exclude = []
@@ -237,251 +257,3 @@ g.set_xticklabels(rotation=45)
 # Add legend with plt.legend, add_legend has a bug
 plt.legend(handles=handles, title="method", loc="upper left", bbox_to_anchor=(1.25,0.9))
 
-
-# + tags=[]
-# patient_df = {}
-# # Plot deviation against prediction day for one patient across all methods
-
-# # Loop through patients
-# for patient_temp in patient_list:
-    
-#     # Extract information from one patient only    
-#     patient_df[patient_temp] = results_df[results_df['patient']==patient_temp]
-
-#     # Set palette
-#     sns.set_palette("Paired")
-
-#     # Plot
-#     plt.figure(figsize=(20,5))
-#     ax = sns.catplot(x="prediction day", y="deviation", hue="method", kind="point", data=patient_df[patient_temp], aspect=1.25)
-#     plt.xticks(rotation=45, horizontalalignment='right', fontweight='light', fontsize=10)
-#     plt.xlabel("Prediction Day")
-#     plt.ylabel("Deviation")
-#     plt.title("Patient " + patient_temp)
-#     # plt.show()
-
-# sub_df = patient_df['132']
-# sub_df[sub_df['method']=='L_PPM_origin_int']
-# # sub_df = results_df[results_df['method']=='L_Cum_origin_int']
-# # sub_df[sub_df['patient']=='132']
-
-# +
-# [patient for patient in patient_list]
-# # ['84', '114', '117', '118', '120', '121', '122', '123', '125', '126', 
-#                # '129', '130', '131', '132', '133', '138']
-
-# + tags=[]
-# # Create dataframe for every method
-# dfnames = ['Q_Cum', 'Q_Cum_origin_int', 'Q_Cum_origin_dp']
-
-# for x in dfnames: exec(x + ' = pd.DataFrame()')
-
-# i = 0
-
-# # Loop through methods
-# method_dataframes = [df_Q_Cum, df_Q_Cum_origin_int, df_Q_Cum_origin_dp,
-#                     df_Q_PPM, df_Q_PPM_origin_int, df_Q_Cum_origin_dp,
-#                     df_Q_RW,
-#                     df_L_Cum, df_L_Cum_origin_int, df_L_Cum_origin_dp,
-#                     df_L_PPM, df_L_PPM_origin_int, df_L_Cum_origin_dp,
-#                     df_L_RW]
-# for dataframe in range(1, len(method_dataframes) - 1):
-
-#     # Loop through patients
-#     for patient in patient_list:
-#         print(i)
-#         i = i + 1
-#         # Add patient column to all sub-dataframes per methods
-#         dataframe[patient].insert(0, 'patient', patient)
-
-#         # Concat sub-dataframes 
-        
-
-# # Write to excel
-
-# print(df_Q_Cum['138'])
-
-# +
-# methods = ['Q_Cum', 'Q_PPM', 'Q_RW', 'L_Cum', 'L_PPM', 'L_RW']
-
-# df_Q_Cum['method'] = 'Q_Cum'
-# df_Q_PPM['method'] = 'Q_PPM'
-# df_Q_RW['method'] = 'Q_RW'
-# df_L_Cum['method'] = 'L_Cum'
-# df_L_PPM['method'] = 'L_PPM'
-# df_L_RW['method'] = 'L_RW'
-# df_Q_Cum_0['method'] = 'Q_Cum_0'
-# df_Q_PPM_0['method'] = 'Q_PPM_0'
-# df_Q_RW_0['method'] = 'Q_RW_0'
-# df_L_Cum_0['method'] = 'L_Cum_0'
-# df_L_PPM_0['method'] = 'L_PPM_0'
-# df_L_RW_0['method'] = 'L_RW_0'
-# df_all_methods = pd.concat([df_Q_Cum, df_Q_PPM, df_Q_RW, df_L_Cum, df_L_PPM, df_L_RW,
-#                            df_Q_Cum_0, df_Q_PPM_0, df_Q_RW_0, df_L_Cum_0, df_L_PPM_0, df_L_RW_0])
-# df_all_methods = df_all_methods.reset_index(drop = True)
-
-# # Remove rows with prediction day 4
-# df_all_methods.drop(df_all_methods[df_all_methods['prediction day'] <= 4].index, inplace=True)
-# df_all_methods = df_all_methods.reset_index(drop = True)
-# df_all_methods.columns = ['pred_day', 'a', 'b', 'c',
-#                                        'prediction', 'deviation', 'abs_dev', 'method']
-
-
-# df_all_methods['method'] = df_all_methods['method'].astype('category')
-# df_all_methods['method'].cat.reorder_categories(['Q_Cum', 'Q_PPM', 'Q_RW', 
-#                                                  'L_Cum', 'L_PPM', 'L_RW',
-#                                                 'Q_Cum_0', 'Q_PPM_0', 'Q_RW_0', 
-#                                                  'L_Cum_0', 'L_PPM_0', 'L_RW_0'])
-
-# # create color mapping based on all unique values of ticker
-# method = df_all_methods.method.unique()
-# colors = sns.color_palette('Paired')  # get a number of colors
-# cmap = dict(zip(method, colors))  # zip values to colors
-
-# # plot
-# plt.figure(figsize=(16, 10))
-# sns.lineplot(x='pred_day', y='deviation', hue='method', data=df_all_methods, palette=cmap)
-
-# # plt.tight_layout()
-# plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
-# plt.xlabel("Day of Prediction")
-# plt.ylabel("Deviation")
-# plt.title("Deviation of Prediction from Actual Value")
-
-# # plt.savefig('test.png', bbox_inches="tight", dpi=300)
-
-# +
-# # # Plot mean deviation
-
-# methods = ['L_Cum', 'L_Cum_0', 'L_PPM', 'L_PPM_0', 'L_RW', 'L_RW_0', 
-#            'Q_Cum', 'Q_Cum_0', 'Q_PPM', 'Q_PPM_0', 'Q_RW', 'Q_RW_0']
-# x_pos = np.arange(len(methods))
-# CTEs = df_all_methods.groupby("method").deviation.mean()
-# error = df_all_methods.groupby("method").deviation.std()
-
-# # Build the plot
-# fig, ax = plt.subplots()
-# ax.bar(x_pos, CTEs, yerr=error, align='center', alpha=0.5, ecolor='black', capsize=10)
-# ax.set_ylabel('Deviation (Mean \u00B1 SD)')
-# ax.set_xticks(x_pos)
-# ax.set_xticklabels(methods)
-# ax.set_title('Deviation of Predicted from Actual Value (Mean \u00B1 SD)')
-# ax.yaxis.grid(True)
-# plt.xticks(rotation=45)
-
-# # Save the figure and show
-# plt.tight_layout()
-# # plt.savefig('all_methods_mean_deviation.png', bbox_inches="tight", dpi=300)
-# plt.show()
-
-# error
-
-# +
-# # Plot median of deviation
-# methods = ['L_Cum', 'L_Cum_0', 'L_PPM', 'L_PPM_0', 'L_RW', 'L_RW_0', 
-#            'Q_Cum', 'Q_Cum_0', 'Q_PPM', 'Q_PPM_0', 'Q_RW', 'Q_RW_0']
-# x_pos = np.arange(len(methods))
-
-# df = df_all_methods.pivot("pred_day", "method" , "deviation")
-
-# data = [df['L_Cum'], df['L_Cum_0'], df['L_PPM'], df['L_PPM_0'], df['L_RW'], df['L_RW_0'], 
-#            df['Q_Cum'], df['Q_Cum_0'], df['Q_PPM'], df['Q_PPM_0'], df['Q_RW'], df['Q_RW_0']]
-
-# fig, ax = plt.subplots()
-# ax.set_title('Deviation of Predicted from Actual Value (Median)')
-# ax.boxplot(data)
-# ax.set_xticklabels(methods)
-# plt.ylabel('Deviation (Median)')
-# plt.xticks(rotation=45)
-# # plt.savefig('all_methods_median_dev.png', bbox_inches="tight", dpi=300)
-# plt.show()
-
-# +
-# # Plot RMSE and MAE
-# df_rmse_MAE = pd.DataFrame()
-
-# ## Plot RMSE
-# methods = ['L_Cum', 'L_Cum_0', 'L_PPM', 'L_PPM_0', 'L_RW', 'L_RW_0', 
-#            'Q_Cum', 'Q_Cum_0', 'Q_PPM', 'Q_PPM_0', 'Q_RW', 'Q_RW_0']
-
-# df = df_all_methods.pivot("pred_day", "method" , "deviation")
-
-# rmse_Q_Cum = np.sqrt(mean(df['Q_Cum']**2))
-# rmse_Q_PPM = np.sqrt(mean(df['Q_PPM']**2))
-# rmse_Q_RW = np.sqrt(mean(df['Q_RW']**2))
-# rmse_L_Cum = np.sqrt(mean(df['L_Cum']**2))
-# rmse_L_PPM = np.sqrt(mean(df['Q_Cum']**2))
-# rmse_L_RW = np.sqrt(mean(df['L_RW']**2))
-# rmse_Q_Cum_0 = np.sqrt(mean(df['Q_Cum_0']**2))
-# rmse_Q_PPM_0 = np.sqrt(mean(df['Q_PPM_0']**2))
-# rmse_Q_RW_0 = np.sqrt(mean(df['Q_RW_0']**2))
-# rmse_L_Cum_0 = np.sqrt(mean(df['L_Cum_0']**2))
-# rmse_L_PPM_0 = np.sqrt(mean(df['Q_Cum_0']**2))
-# rmse_L_RW_0 = np.sqrt(mean(df['L_RW_0']**2))
-
-# rmse = np.array([rmse_L_Cum, rmse_L_Cum_0, rmse_L_PPM, rmse_L_PPM_0, rmse_L_RW, rmse_L_RW_0,
-#                 rmse_Q_Cum, rmse_Q_Cum_0, rmse_Q_PPM, rmse_Q_PPM_0, rmse_Q_RW, rmse_Q_RW_0])
-
-# rmse = pd.DataFrame(rmse.reshape(-1, len(rmse)),columns=methods)
-# rmse=rmse.transpose()
-
-# ## Calculate MAE
-# MAE_Q_Cum = mean(abs(df['Q_Cum']))
-# MAE_Q_PPM = mean(abs(df['Q_PPM']))
-# MAE_Q_RW = mean(abs(df['Q_RW']))
-# MAE_L_Cum = mean(abs(df['L_Cum']))
-# MAE_L_PPM = mean(abs(df['L_PPM']))
-# MAE_L_RW = mean(abs(df['L_RW']))
-# MAE_Q_Cum_0 = mean(abs(df['Q_Cum_0']))
-# MAE_Q_PPM_0 = mean(abs(df['Q_PPM_0']))
-# MAE_Q_RW_0 = mean(abs(df['Q_RW_0']))
-# MAE_L_Cum_0 = mean(abs(df['L_Cum_0']))
-# MAE_L_PPM_0 = mean(abs(df['L_PPM_0']))
-# MAE_L_RW_0 = mean(abs(df['L_RW_0']))
-
-# MAE = np.array([MAE_L_Cum, MAE_L_Cum_0, MAE_L_PPM, MAE_L_PPM_0, MAE_L_RW, MAE_L_RW_0,
-#                 MAE_Q_Cum, MAE_Q_Cum_0, MAE_Q_PPM, MAE_Q_PPM_0, MAE_Q_RW, MAE_Q_RW_0])
-
-# MAE = pd.DataFrame(MAE.reshape(-1, len(MAE)),columns=methods)
-# MAE=MAE.transpose()
-
-# df_rmse_MAE = df_rmse_MAE.append(rmse)
-# df_rmse_MAE = pd.concat([df_rmse_MAE, MAE], axis=1)
-# df_rmse_MAE.columns = ['RMSE', 'MAE']
-
-# df_rmse_MAE.index=['L_Cum', 'L_Cum_0', 'L_PPM', 'L_PPM_0', 'L_RW', 'L_RW_0', 
-#            'Q_Cum', 'Q_Cum_0', 'Q_PPM', 'Q_PPM_0', 'Q_RW', 'Q_RW_0']
-
-# df_rmse_MAE.plot()
-# plt.xticks(np.arange(len(df_rmse_MAE.index)), df_rmse_MAE.index, rotation=45)
-
-# plt.ylabel('RMSE and MAE')
-# plt.title("RMSE and MAE of Deviation of Predicted from Actual Value")
-# plt.xticks(rotation=45)
-# # plt.savefig('all_methods_MAE_RMSE.png', bbox_inches="tight", dpi=300)
-
-# +
-# # Plot prediction of all methods
-# pred = df_all_methods.pivot("pred_day", "method", "prediction")
-# pred['L_Cum'].plot()
-# pred['L_Cum_0'].plot()
-# pred['L_PPM'].plot()
-# pred['L_PPM_0'].plot()
-# pred['L_RW'].plot()
-# pred['L_RW_0'].plot()
-# pred['Q_Cum'].plot()
-# pred['Q_Cum_0'].plot()
-# pred['Q_PPM'].plot()
-# pred['Q_PPM_0'].plot()
-# pred['Q_RW'].plot()
-# pred['Q_RW_0'].plot()
-# plt.axhline(y = 8, color = "black", linestyle = '--')
-# plt.axhline(y = 10, color = "black", linestyle = '--')
-# plt.legend()
-# plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
-# plt.xlabel("Day of Prediction")
-# plt.ylabel("Prediction")
-# plt.title("Prediction of Tac Level")
-# # plt.title("Prediction of Tac Level (without Q_PPM, Q_PPM_0, Q_RW, Q_RW_0)")
-# plt.tight_layout()
-# # plt.savefig('prediction.png', bbox_inches="tight", dpi=300)
